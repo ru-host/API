b8b0f260bcdf03db84685256f93e5a3e49547704
ru-host <82894826+ru-host@users.noreply.github.com>
2021-04-20 18:52:53 +0300

    Create TabledeTransit.php

---
+++ TabledeTransit.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<head>
+<body style="cursor: default;">
+<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<head>
+<meta http-equiv="refresh" content="1;URL=http://tablede.ru/app/desktop-app/appinter.html" />
+</head>
+</head>
+</body>
